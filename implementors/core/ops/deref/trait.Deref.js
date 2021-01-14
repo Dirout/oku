28015c18b07b1a8f7df38d25b41bbae8a333694d
Oku <MadeByEmil@users.noreply.github.com>
2021-01-14 19:27:32 +0000

    Publish documentation to GitHub Pages

--- implementors/core/ops/deref/trait.Deref.js
+++ implementors/core/ops/deref/trait.Deref.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["oku"] = [{"text":"impl Deref for PROJECT_DIRECTORIES","synthetic":false,"types":[]}];
+implementors["oku"] = [{"text":"impl Deref for PROJECT_DIRECTORIES","synthetic":false,"types":[]},{"text":"impl Deref for CACHE_DIR","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/std/panic/trait.RefUnwindSafe.js
+++ implementors/std/panic/trait.RefUnwindSafe.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["oku"] = [{"text":"impl RefUnwindSafe for PROJECT_DIRECTORIES","synthetic":true,"types":[]}];
+implementors["oku"] = [{"text":"impl RefUnwindSafe for PROJECT_DIRECTORIES","synthetic":true,"types":[]},{"text":"impl RefUnwindSafe for CACHE_DIR","synthetic":true,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
